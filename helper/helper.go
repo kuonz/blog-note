d6f9d404c256e484328f226f0d4ff62db16fcf16
kuonz <kuonz@outlook.com>
2020-04-06 20:04:03 +0800

    修改缩进为2个空格

--- helper/helper.go
+++ helper/helper.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"regexp"
-	"strings"
-	"time"
+  "fmt"
+  "io/ioutil"
+  "regexp"
+  "strings"
+  "time"
 )
 
 var exp1 = regexp.MustCompile(`(?U)!\[.*\]\(.*\)`)
@@ -16,92 +16,92 @@ var staticPath = ""
 var processCount = 0
 
 func main() {
-	fmt.Println("将处理缩进、头部信息、图片地址")
+  fmt.Println("将处理缩进、头部信息、图片地址")
 
-	traversal("..", "")
+  traversal("..", "")
 
-	fmt.Printf("process %d article.\n", processCount)
+  fmt.Printf("process %d article.\n", processCount)
 }
 
 func traversal(prefix string, fileName string) {
 
-	currentPath := prefix + fileName + "/"
+  currentPath := prefix + fileName + "/"
 
-	infos, _ := ioutil.ReadDir(currentPath)
+  infos, _ := ioutil.ReadDir(currentPath)
 
-	for _, item := range infos {
-		itemName := item.Name()
+  for _, item := range infos {
+    itemName := item.Name()
 
-		if item.IsDir() {
-			traversal(currentPath, itemName)
-		} else {
-			if strings.HasSuffix(itemName, ".md") {
-				titleName := getTitleName(itemName)
-				processMD(currentPath, itemName, titleName)
-			}
-		}
-	}
+    if item.IsDir() {
+      traversal(currentPath, itemName)
+    } else {
+      if strings.HasSuffix(itemName, ".md") {
+        titleName := getTitleName(itemName)
+        processMD(currentPath, itemName, titleName)
+      }
+    }
+  }
 }
 
 func processMD(filePath string, fileName string, titleName string) {
 
-	contentByte, _ := ioutil.ReadFile(filePath + "/" + fileName)
-	contentString := string(contentByte)
+  contentByte, _ := ioutil.ReadFile(filePath + "/" + fileName)
+  contentString := string(contentByte)
 
-	if !strings.HasPrefix(contentString, `---`) {
-		staticPath = strings.ReplaceAll(filePath, "..", "(/post")
+  if !strings.HasPrefix(contentString, `---`) {
+    staticPath = strings.ReplaceAll(filePath, "..", "(/post")
 
-		currentDate := time.Now().Format("2006-01-02")
+    currentDate := time.Now().Format("2006-01-02")
 
-		temp := strings.Split(filePath, "/")
-		currentCatogories := getTitleName(temp[len(temp)-2])
+    temp := strings.Split(filePath, "/")
+    currentCatogories := getTitleName(temp[len(temp)-2])
 
-		processContent(&contentString, titleName, currentDate, currentCatogories)
+    processContent(&contentString, titleName, currentDate, currentCatogories)
 
-		ioutil.WriteFile(filePath+"/"+fileName, []byte(contentString), 0666)
+    ioutil.WriteFile(filePath+"/"+fileName, []byte(contentString), 0666)
 
-		fmt.Printf("processed %s ... \n", filePath + "/" + fileName)
-		processCount++
-	}
+    fmt.Printf("processed %s ... \n", filePath + "/" + fileName)
+    processCount++
+  }
 }
 
 func processContent(src *string, title, date, categories string) {
 
-	// 添加头部信息
-	tl := `---
+  // 添加头部信息
+  tl := `---
 author: "kuonz"
 draft: false
 title: "` + title + `"
 date: ` + date + `
 categories: ["` + categories + `"]
 ---
-	
+  
 `
-	*src = tl + *src
+  *src = tl + *src
 
-	// 处理缩进
-	*src = strings.ReplaceAll(*src, "\t", "  ")
+  // 处理缩进
+  *src = strings.ReplaceAll(*src, "\t", "  ")
 
-	// 处理图片
-	*src = exp1.ReplaceAllStringFunc(*src, processImage)
+  // 处理图片
+  *src = exp1.ReplaceAllStringFunc(*src, processImage)
 }
 
 func processImage(src string) string {
-	return exp3.ReplaceAllStringFunc(exp2.ReplaceAllString(src, "[]"), processImage2)
+  return exp3.ReplaceAllStringFunc(exp2.ReplaceAllString(src, "[]"), processImage2)
 }
 
 func processImage2(src string) string {
-	return strings.ReplaceAll(src, "(", staticPath)
+  return strings.ReplaceAll(src, "(", staticPath)
 }
 
 func getTitleName(fileName string) string {
-	index := strings.Index(fileName, "-")
-	if index > 0 {
-		fileName = fileName[index+1:]
-	}
-	index = strings.LastIndex(fileName, ".md")
-	if index > 0 {
-		fileName = fileName[:index]
-	}
-	return fileName
+  index := strings.Index(fileName, "-")
+  if index > 0 {
+    fileName = fileName[index+1:]
+  }
+  index = strings.LastIndex(fileName, ".md")
+  if index > 0 {
+    fileName = fileName[:index]
+  }
+  return fileName
 }
